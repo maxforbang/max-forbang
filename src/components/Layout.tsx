fc4614a2725fead1ae67bfc69c350768ee0ef72f
Max Forbang <max@Maxs-MacBook-Pro-2.local>
2023-10-21 07:45:05 -0400

    favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = ({ children }: { children: ReactNode }) => {
           name="description"
           content="Code. Design. Brand. Transform your offer using software. Transform your lead flow using marketing."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <div className="min-h-full">
         <Header scrollToContact={scrollToContact} />
